2a3f07552aab176beb2b04992b0bf3b08382dbb1
Greg Fleming <gfleming@ctmtg.com>
2014-04-02 20:43:16 -0400

    first commit

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+javascript: (function () { 
+  var jsCode = document.createElement('script'); 
+  jsCode.setAttribute('src', 'https://github.com/covaithe/2048bot/bot.js');                  
+  document.body.appendChild(jsCode); 
+}());
