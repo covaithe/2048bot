df1a1a96e7414c43e0f0aa63bb85d74b8b6ec99b
Greg Fleming <gfleming@ctmtg.com>
2014-04-04 08:38:53 -0400

    try to list current cells

--- bot.js
+++ bot.js
@@ -1,3 +1,33 @@
 (function() {
-  alert('hi');
+  var Grid = function() {
+    this.cells = new Array(16);
+
+    this.getCell = function(x,y) {
+      return cells[ cellIndex(x,y) ];
+    };
+    this.setCell = function(x,y,value) {
+      cells[ cellIndex(x,y) ] = value;
+    };
+
+    function cellIndex(x,y) {
+      x = x - 1;
+      y = y - 1;
+      return x*4 + y;
+    }
+  };
+
+
+  var container = new HTMLActuator().tileContainer;
+  var grid = new Grid();
+
+  for(var i = 0; i < container.children.length; i++) {
+    cell = container.children[i];
+    classes = cell.classList;
+    value = intParse(classes[1].split('-')[1]);
+    position = classes[2].split('-');
+    x = position[2];
+    y = position[3];
+    grid.setCell(x,y,value);
+  }
+  console.log(grid);
 })();
